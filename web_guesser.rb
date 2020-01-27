083f7bd82b7388b5e807fb32647eb2962cc83e57
Varras <cassmcd41@gmail.com>
2020-01-26 18:23:14 -0800

    I2.1: inserted messaging

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,30 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+def check_guess(guess)
+    if params["guess"].to_i == guess
+        "You got it right!"        
+    elsif params["guess"].to_i > guess
+        if params["guess"].to_i > (guess + 5)
+            "Way to high!"
+        else
+            "Close, but too high!"
+        end
+    elsif params["guess"].to_i < guess
+        if params["guess"].to_i <(guess - 5)
+            "Way to low!"
+        else
+            "Close, but too low!"
+        end
+        
+    end
+end
+
+
 number = rand(101)
 get '/' do
-    erb :index, :locals => {:number => number}
+    guess = params["guess"].to_i
+    message = check_guess(number)
+    erb :index, :locals => {:number => number, :message => message }    
+
 end
